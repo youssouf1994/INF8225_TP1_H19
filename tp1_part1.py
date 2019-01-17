d2056ddf2fbcf199ead15408dc69859cf2796aca
youssouf1994 <youssouf1994@github.com>
2019-01-17 14:53:54 -0500

    Add comments to the tp1_part1.py

--- tp1_part1.py
+++ tp1_part1.py
@@ -7,25 +7,33 @@ import matplotlib.pyplot as plt
 
 prob_pluie = np.array([0.8, 0.2]).reshape(2, 1, 1, 1)
 print("Pr(Pluie)={}\n".format(np.squeeze(prob_pluie)))
+
 prob_arroseur = np.array([0.9, 0.1]).reshape(1, 2, 1, 1)
 print("Pr(Arroseur)={}\n".format(np.squeeze(prob_arroseur)))
+
 watson = np.array([[0.8, 0.2], [0, 1]]).reshape(2, 1, 2, 1)
 print("Pr(Watson | Pluie)={}\n".format(np.squeeze(watson)))
+
 holmes = np.array([[1, 0], [0.1, 0.9], [0, 1], [0, 1]]).reshape(2, 2, 1, 2)
 print("Pr(Holmes | Pluie, Arroseur)={}\n".format(np.squeeze(holmes)))
 
-a = (watson * prob_pluie).sum(0).squeeze()[1] # prob gazon watson mouille
+# prob gazon watson mouille.
+a = (watson * prob_pluie)[:,:,1,:].sum(0).squeeze()
 print("Pr(Watson=1)={}\n".format(a))
 
-b = ((watson * holmes * prob_pluie * prob_arroseur).sum((0, 1)).squeeze()[1, 1]) /((holmes * prob_pluie * prob_arroseur).sum((0, 1)).squeeze()[1]) 
+# prob gazon watson mouille sachant que gazon holmes mouille.
+b = ((watson * holmes * prob_pluie * prob_arroseur)[:,:,1,1].sum((0, 1)).squeeze()) \
+    /((holmes * prob_pluie * prob_arroseur)[:,:,:,1].sum((0, 1)).squeeze())
 print("Pr(Watson=1 | Holmes=1)={}\n".format(b))
 
-c = ((watson * holmes[:,0,:,:].reshape(2,1,1,2) * prob_pluie).sum(0).squeeze()[1, 1])/((holmes[:,0,:,:].reshape(2,1,1,2) * prob_pluie).sum((0)).squeeze()[1])
+# prob gazon watson mouille sachat que gazon holmes mouille et arroseur ne fonctionne pas.
+c = ((watson * holmes * prob_pluie)[:,0,1,1].sum(0).squeeze())/((holmes * prob_pluie)[:,0,:,1].sum(0).squeeze())
 print("Pr(Watson=1 | Holmes=1, Arroseur=0)={}\n".format(c))
 
-d = a
+# prob gazon watson mouille sachant que arroseur ne fonctionne pas.
+d = (watson * prob_pluie)[:,0,1,:].sum(0).squeeze()
 print("Pr(Watson=1 | Arroseur=0)={}\n".format(d))
 
+# prob gazon watson mouille sachant que pluie tombe.
 e = watson[1,:,1,:].squeeze()
 print("Pr(Watson=1 | Pluie=1)={}\n".format(e))
-
